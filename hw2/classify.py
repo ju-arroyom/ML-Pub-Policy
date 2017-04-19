32037dcae096f6fa4ffada9e1ed05796e75436e6
Anne Rogers <amr@cs.uchicago.edu>
2017-04-18 22:13:58 -0500

    Done with Hw2

--- hw2/classify.py
+++ hw2/classify.py
@@ -5,6 +5,7 @@ import numpy as np
 import seaborn as sn
 import matplotlib.pyplot as plt
 import matplotlib.ticker as tkr
+import pylab as pl
 from sklearn import preprocessing
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
@@ -12,12 +13,23 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 from sklearn.feature_selection import RFE
 from sklearn import tree
+from sklearn.metrics import roc_curve, auc
 
 import warnings
 warnings.filterwarnings("ignore")
 
+def dummify(data, col_name):
+    '''
+    Given a pandas dataframe and a column name
+    creates dummy variables indicating whether the
+    category was present or not.
+    '''
+    dummy = pd.get_dummies(data[col_name])
+    df_add_dummy = pd.concat([data, dummy], axis =1 )
+    return df_add_dummy
 
-def do_learning(X_training, Y_training, X_test, Y_test, reference_dic, model_class):
+
+def do_learning(X_training, Y_training, X_test, Y_test, model_class):
 
     '''
     With training and testing data select the best
@@ -25,7 +37,10 @@ def do_learning(X_training, Y_training, X_test, Y_test, reference_dic, model_cla
     fit a classifier and return a tuple containing the predicted values on the test data
     and a list of the best features used.
     '''
-    
+    ref_dic = {}
+    for index, x in enumerate(X_training.columns):
+        ref_dic[index] = x
+
     model = model_class
     # Recursive Feature Elimination
     rfe = RFE(model)
@@ -33,13 +48,13 @@ def do_learning(X_training, Y_training, X_test, Y_test, reference_dic, model_cla
     
     best_features = rfe.get_support(indices=True)
 
-    best_features_names = [reference_dic[i] for i in best_features]
+    best_features_names = [ref_dic[i] for i in best_features]
 
     predicted = rfe.predict(X_test)
     expected = Y_test
 
     accuracy = accuracy_score(expected, predicted)
-    return (expected, predicted, best_features_names, accuracy)
+    return (rfe, expected, predicted, best_features_names, accuracy)
 
 
 def plot_confusion_matrix(data, label_list, model_name):
@@ -55,3 +70,25 @@ def plot_confusion_matrix(data, label_list, model_name):
     sn.heatmap(data, annot=True,annot_kws={"size": 16}, linewidths=.5, xticklabels = xticks,  
               yticklabels = yticks, fmt = '')
     ax.set_title('Confusion Matrix for' + ' ' + model_name)
+
+
+def plot_roc(model_class, X_test, Y_test):
+    '''
+    Given the results stored in rfe and a testing
+    set, the function plots the roc curve
+    '''
+    preds = model_class.predict_proba(X_test)
+    fpr, tpr, thresholds = roc_curve(Y_test, preds[:, 1])
+    roc_auc = auc(fpr, tpr)
+
+    # Plot ROC curve
+    pl.clf()
+    pl.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
+    pl.plot([0, 1], [0, 1], 'k--')
+    pl.xlim([0.0, 1.0])
+    pl.ylim([0.0, 1.0])
+    pl.xlabel('False Positive Rate')
+    pl.ylabel('True Positive Rate')
+    pl.title('Receiver operating characteristic example')
+    pl.legend(loc="lower right")
+    pl.show()
