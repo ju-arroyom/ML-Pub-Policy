b424c2033829bf96812e5d1c113682e59edf34e1
Anne Rogers <amr@cs.uchicago.edu>
2017-05-03 21:31:07 -0500

    Confusion Party in my magic loop

--- hw3/classify.py
+++ hw3/classify.py
@@ -1,35 +1,240 @@
+# Import pre-processing and plotting libraries
+import time
 import read_files
 import process_data
 import pandas as pd
 import numpy as np
-import seaborn as sn
+import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.ticker as tkr
 import pylab as pl
+# Import models and metrics from sklearn
 from sklearn import preprocessing
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import *
+from sklearn.model_selection import ParameterGrid, train_test_split
+
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier, GradientBoostingClassifier, AdaBoostClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import confusion_matrix
-from sklearn.feature_selection import RFE
-from sklearn import tree
-from sklearn.metrics import roc_curve, auc
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics.classification import _check_targets
+
+
 
 import warnings
 warnings.filterwarnings("ignore")
 
-def dummify(data, col_name):
-    '''
-    Given a pandas dataframe and a column name
-    creates dummy variables indicating whether the
-    category was present or not.
-    '''
-    dummy = pd.get_dummies(data[col_name])
-    df_add_dummy = pd.concat([data, dummy], axis =1 )
-    return df_add_dummy
+
+def define_clfs_params(grid_size):
+
+    """
+    Defines classifiers and param grid that are used
+    to find best model.
+    Adjusted from: https://github.com/rayidghani/magicloops/blob/master/magicloops.py
+    """
+
+    clfs = {
+        'LR': LogisticRegression(penalty='l1', C=1e5),
+        'KNN': KNeighborsClassifier(n_neighbors=3),
+        'DT': DecisionTreeClassifier(),
+        'SVM': svm.SVC(kernel='linear', probability=True, random_state=0), 
+        'RF': RandomForestClassifier(n_estimators=50, n_jobs=-1),
+        'AB': AdaBoostClassifier(DecisionTreeClassifier(max_depth=1), algorithm="SAMME", n_estimators=200),
+        'GB': GradientBoostingClassifier(learning_rate=0.05, subsample=0.5, max_depth=6, n_estimators=10),
+        'ET': ExtraTreesClassifier(n_estimators=10, n_jobs=-1, criterion='entropy'),
+        'NB': GaussianNB(),  
+            }
+    
+    small_grid = { 
+    'RF':{'n_estimators': [10,100], 'max_depth': [5,50], 'max_features': ['sqrt','log2'],'min_samples_split': [2,10]},
+    'LR': { 'penalty': ['l1','l2'], 'C': [0.00001,0.001,0.1,1,10]},
+    'ET': { 'n_estimators': [10,100], 'criterion' : ['gini', 'entropy'] ,'max_depth': [5,50], 'max_features': ['sqrt','log2'],'min_samples_split': [2,10]},
+    'AB': { 'algorithm': ['SAMME', 'SAMME.R'], 'n_estimators': [1,10,100,1000,10000]},
+    'GB': {'n_estimators': [10,100], 'learning_rate' : [0.001,0.1,0.5],'subsample' : [0.1,0.5,1.0], 'max_depth': [5,50]},
+    'NB' : {},
+    'DT': {'criterion': ['gini', 'entropy'], 'max_depth': [1,5,10,20,50,100], 'max_features': ['sqrt','log2'],'min_samples_split': [2,5,10]},
+    'SVM' :{'C' :[0.00001,0.0001,0.001,0.01,0.1,1,10],'kernel':['linear']},
+    'KNN' :{'n_neighbors': [1,5,10,25,50,100],'weights': ['uniform','distance'],'algorithm': ['auto','ball_tree','kd_tree']}
+           }
+    
+    test_grid = { 
+    'RF':{'n_estimators': [1], 'max_depth': [1], 'max_features': ['sqrt'],'min_samples_split': [10]},
+    'LR': { 'penalty': ['l1'], 'C': [0.01]},
+    'ET': { 'n_estimators': [1], 'criterion' : ['gini'] ,'max_depth': [1], 'max_features': ['sqrt'],'min_samples_split': [10]},
+    'AB': { 'algorithm': ['SAMME'], 'n_estimators': [1]},
+    'GB': {'n_estimators': [1], 'learning_rate' : [0.1],'subsample' : [0.5], 'max_depth': [1]},
+    'NB' : {},
+    'DT': {'criterion': ['gini'], 'max_depth': [1], 'max_features': ['sqrt'],'min_samples_split': [10]},
+    'SVM' :{'C' :[0.01],'kernel':['linear']},
+    'KNN' :{'n_neighbors': [5],'weights': ['uniform'],'algorithm': ['auto']}
+           }
+    
+    if (grid_size == 'small'):
+        return clfs, small_grid
+    elif (grid_size == 'test'):
+        return clfs, test_grid
+    else:
+        return 0, 0
+
+        
 
 
-def do_learning(X_training, Y_training, X_test, Y_test, model_class):
+
+def clf_loop(models_to_run, clfs, grid, X, y , print_plots = False):
+    """
+    Loops through classifiers and stores metrics in pandas df.
+    Df gets returned.
+
+    Adjusted from: https://github.com/rayidghani/magicloops/blob/master/magicloops.py
+    In:
+        - models_to_run: (list) of models to run
+        - clfs: (dict) of classifiers
+        - grid: (dict) of classifiers with set of parameters to train on
+        - X_train: features from training set
+        - X_test: features from test set
+        - y_train: targets of training set
+        - y_test: targets of test set
+        - print_plots: (bool) whether or not to print plots
+    Out:
+        - pandas df
+    """
+
+    conf_matrix = {}
+    count = 1
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+    results_df = pd.DataFrame(columns=('model_type', 'clf', 'parameters', 'train_time',
+                                        'predict_time', 'auc-roc', 'p_at_5', 
+                                        'p_at_10', 'p_at_20'))
+
+    for index, clf in enumerate([clfs[x] for x in models_to_run]):
+
+        
+
+        print(models_to_run[index])
+
+        parameter_values = grid[models_to_run[index]]
+        for p in ParameterGrid(parameter_values):
+            try:
+                clf.set_params(**p)
+
+                start_time_training = time.time()
+                clf.fit(X_train, y_train)
+                train_time = time.time() - start_time_training
+
+                start_time_predicting = time.time()
+
+
+                y_pred_probs = clf.fit(X_train, y_train).predict_proba(X_test)[:,1]
+
+
+                predict_time = time.time() - start_time_predicting
+
+                roc_score = roc_auc_score(y_test, y_pred_probs)
+                y_pred_probs_sorted, y_test_sorted = zip(*sorted(zip(y_pred_probs, y_test), reverse=True))
+
+   
+
+                results_df.loc[len(results_df)] = [models_to_run[index], clf, p,
+                                                   train_time, predict_time, roc_score, 
+                                                   precision_at_k(y_test_sorted,y_pred_probs_sorted,5.0),
+                                                   precision_at_k(y_test_sorted,y_pred_probs_sorted,10.0),
+                                                   precision_at_k(y_test_sorted,y_pred_probs_sorted,20.0)]
+
+                if print_plots:
+
+                    plot_precision_recall_n(y_test, y_pred_probs, clf)
+
+                    cm = confusion_matrix(y_test, clf.fit(X_train, y_train).predict(X_test))
+
+                    if count not in conf_matrix:
+                        conf_matrix[count] = {'matrix' : cm , 'title': models_to_run[index]}
+
+                count = count +1
+
+            except IndexError as e:
+                print('Error:', e)
+                continue
+
+    return results_df, conf_matrix
+
+
+def generate_binary_at_k(y_scores, k):
+    cutoff_index = int(len(y_scores) * (k / 100.0))
+    test_predictions_binary = [1 if x < cutoff_index else 0 for x in range(len(y_scores))]
+    return test_predictions_binary
+
+def precision_at_k(y_true, y_scores, k):
+    preds_at_k = generate_binary_at_k(y_scores, k)
+    #precision, _, _, _ = metrics.precision_recall_fscore_support(y_true, preds_at_k)
+    #precision = precision[1]  # only interested in precision for label 1
+    precision = precision_score(y_true, preds_at_k)
+    return precision
+
+
+
+
+
+
+def plot_precision_recall_n(y_true, y_prob, model_name):
+
+    """
+    Function to plot precision recall curve.
+    Adjusted from: https://github.com/rayidghani/magicloops/blob/master/magicloops.py
+    """
+
+    y_score = y_prob
+    precision_curve, recall_curve, pr_thresholds = precision_recall_curve(y_true, y_score)
+    precision_curve = precision_curve[:-1]
+    recall_curve = recall_curve[:-1]
+    pct_above_per_thresh = []
+    number_scored = len(y_score)
+
+    for value in pr_thresholds:
+        num_above_thresh = len(y_score[y_score>=value])
+        pct_above_thresh = num_above_thresh / float(number_scored)
+        pct_above_per_thresh.append(pct_above_thresh)
+
+    pct_above_per_thresh = np.array(pct_above_per_thresh)
+    
+    plt.clf()
+    fig, ax1 = plt.subplots()
+    ax1.plot(pct_above_per_thresh, precision_curve, 'b')
+    ax1.set_xlabel('percent of population')
+    ax1.set_ylabel('precision', color='b')
+    ax2 = ax1.twinx()
+    ax2.plot(pct_above_per_thresh, recall_curve, 'r')
+    ax2.set_ylabel('recall', color='r')
+    ax1.set_ylim([0,1])
+    ax1.set_ylim([0,1])
+    ax2.set_xlim([0,1])
+    
+    name = model_name
+    plt.title(name)
+    #plt.savefig(name)
+    plt.show()
+
+
+
+
+def do_learning(features_list, grid_size, X_data, target):
+    '''
+    '''
+    clfs, grid = define_clfs_params(grid_size)
+    models_to_run=['RF','DT','KNN', 'ET', 'AB', 'GB', 'LR', 'NB']
+
+    results_df, cm_dic = clf_loop(models_to_run, clfs,grid, X_data, target, True)
+
+    results_df.to_csv('results.csv', index=False)
+
+    return cm_dic
+
+
+def do(X_data, target, model_list, hyperparameters):
 
     '''
     With training and testing data select the best
@@ -62,33 +267,35 @@ def plot_confusion_matrix(data, label_list, model_name):
     Given a pandas dataframe with a confusion confusion_matrix
     and a list of axis lables plot the results
     '''
-    sn.set(font_scale=1.4)#for label size
+    sns.set(font_scale=1.4)#for label size
 
     xticks =  label_list
     yticks =  label_list
     ax = plt.axes()
-    sn.heatmap(data, annot=True,annot_kws={"size": 16}, linewidths=.5, xticklabels = xticks,  
+    sns.heatmap(data, annot=True,annot_kws={"size": 16}, linewidths=.5, xticklabels = xticks,  
               yticklabels = yticks, fmt = '')
     ax.set_title('Confusion Matrix for' + ' ' + model_name)
 
 
-def plot_roc(model_class, X_test, Y_test):
+def confusion_party(matrix_dictionary, label_list):
     '''
-    Given the results stored in rfe and a testing
-    set, the function plots the roc curve
+    Produces the subplots with the confusion matrices
+    for each model and set of parameters.
+    Inputs: A dictionary containing the confusion
+            matrices for the models
+            A list of labels for the axis
+    Returns:
+            Confusion party 
     '''
-    preds = model_class.predict_proba(X_test)
-    fpr, tpr, thresholds = roc_curve(Y_test, preds[:, 1])
-    roc_auc = auc(fpr, tpr)
-
-    # Plot ROC curve
-    pl.clf()
-    pl.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
-    pl.plot([0, 1], [0, 1], 'k--')
-    pl.xlim([0.0, 1.0])
-    pl.ylim([0.0, 1.0])
-    pl.xlabel('False Positive Rate')
-    pl.ylabel('True Positive Rate')
-    pl.title('Receiver operating characteristic example')
-    pl.legend(loc="lower right")
-    pl.show()
+    import math
+    fix, ax = plt.subplots(figsize=(16, 12))
+    plt.suptitle('Confusion Matrix of Various Classifiers')
+    for key, values in matrix_dictionary.items():
+        matrix = values['matrix']
+        title = values['title']
+        plt.subplot(3, math.ceil(len(matrix_dictionary)/3), key) # starts from 1
+        plt.title(str(key)+ " : " +title);
+        xticks =  label_list
+        yticks =  label_list
+        sns.heatmap(matrix, annot=True,annot_kws={"size": 16}, linewidths=.5, xticklabels = xticks,  
+                  yticklabels = yticks, fmt = '');
--- hw3/process_data.py
+++ hw3/process_data.py
@@ -1,19 +1,86 @@
 
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def percentage_missing(data):
+    '''
+    Computes the percentage of missing values by
+    column in the dataframe
+    Input: dataframe
+    Return: prints percentage of missing values by column
+    '''
+    for c in data.columns:
+        if data[c].count() < len(data):
+            missing_perc = ((len(data) - data[c].count()) / float(len(data))) * 100.0
+            print("%.1f%% missing from: Column %s" %(missing_perc, c))
+
+
+def frequency_plots(data, col_name):
+    '''
+    Creates a plot to inspect the distribution of a variable
+    Inputs:
+            data - pandas dataframe
+            col_name - column name
+    Returns: 
+            plot
+    '''
+    fig, (ax1, ax2) = plt.subplots(1,2, figsize=(16,8))
+
+    data[col_name].plot(kind = "hist", alpha = 0.2, bins = 20, color ='r', ax = ax1); 
+    ax1.set_title('Distribution of Number of Dependents');
+
+    sns.boxplot(data[col_name], ax = ax2, palette="Set3"); 
+    ax2.set_title('Boxplot of Number of Dependents')
+
+    plt.tight_layout()
+
+def plot_income_distribution(data, col_name):
+    '''
+    Create subplots for income distribution
+
+    Input: data - pandas dataframe
+           col_name - column name
+    Returns:
+            Income distribution plot
+    '''
+
+    fig, ((ax1, ax2), (ax3, ax4))= plt.subplots(2,2, figsize=(10,10))
+    # Overall Income Distribution
+    sns.boxplot(data[col_name], ax = ax1); ax1.set_title('Overall income distribution')
+    # Up to 80th percentile
+    data.monthlyincome[(data[col_name]<=54166)].plot(kind = "hist", alpha = 0.2, color ='blue', bins= 20, ax = ax2); 
+    ax2.set_title('Monthly Income Dist. up to 80th percentile');
+
+
+    # Monthly income above $54,166 and below $1,000,000
+    data.monthlyincome[(data[col_name]> 54166) & (data[col_name]<=1000000)].plot(kind = "hist", alpha = 0.2, color ='r', bins= 20, ax = ax3); 
+    ax3.set_title('Monthly Income Dist. > 80th percentile < $1,000,000');
+    ax3.ticklabel_format(style='sci', axis='x', scilimits=(0,0));
+    ax3.set_xlabel('Income in Sci Notation')
+
+    # Income distribution above $1,000,000
+    data.monthlyincome[(data[col_name]>1000000)].plot(kind = "hist", alpha = 0.2, color ='black', bins= 20, ax = ax4); 
+    ax4.set_title('Monthly Income Dist. above $1,000,000');
+    ax4.ticklabel_format(style='sci', axis='x', scilimits=(0,0));
+    ax4.set_xlabel('Income in Sci Notation')
+
+    plt.tight_layout()
 
 
 def des_num_dep (data):
     '''
     Creates data frame with cumsum and percentage of 
     dependents by category
+
     Input: pandas data frame object
     Returns: new df with descriptive stats
     '''
     data = data['numberofdependents'].value_counts().to_frame()
-    data['cumsum'] = data['numberofdependents'].cumsum()
+    data['Cumulative_Sum'] = data['numberofdependents'].cumsum()
     total = data['numberofdependents'].sum()
-    data['percentage'] = (data['cumsum'] / total)*100 
+    data['Percentage_By_Group'] = (data['Cumulative_Sum'] / total)*100 
 
     return data
 
@@ -26,6 +93,17 @@ def impute_val_to_column(data, col, method, l_bound = 0, u_bound = 1, freq_list 
     This function imputes specific columns, for imputing all
     columns of the dataset that have missing data, use
     impute_missing_all.
+
+    Input: data - pandas dataframe
+           col - column name
+           method - median, mode, random
+           l_bound - lower bound for range in random imputation (default = 0)
+           u_bound - upper bound for range in random imputation (default = 0)
+           freq_list - list containing the empirical frequency of values in the data 
+                     (defalt = [])
+    Returns:
+            dataframe with imputed values
+
     '''
 
     if method == 'median':
@@ -42,9 +120,14 @@ def discretize(data, column, bins):
     '''
     Given a continuous variable, create a new column in the dataframe
     that represents the bin in which the continuous variable falls into.
-    If verbose is True, print the value counts of each bin.
-    Returns the name of the new column to include programmatically in list of features
+    
+    Inputs: data - pandas dataframe
+            column - column name
+            bins - a range to discretize the data
+
+    Returns: the column with the buckets
     '''
+
     new_col = 'bins_' + str(column)
 
     data[new_col] = pd.cut(data[column], bins=bins)
@@ -52,3 +135,60 @@ def discretize(data, column, bins):
     return data[new_col]
 
 
+
+def dummify(data, col_name):
+    '''
+    Given a pandas dataframe and a column name
+    creates dummy variables indicating whether the
+    category was present or not.
+
+    Inputs: data - pandas dataframe
+            col_name - column name
+
+    Returns: modified dataframe with dummy variables
+    '''
+    dummy = pd.get_dummies(data[col_name])
+    df_add_dummy = pd.concat([data, dummy], axis =1 )
+    return df_add_dummy
+
+def visualize_buckets(data, f1, f2, f3, age_bin, income_bin):
+    '''
+    Plot features by buckets
+    Inputs: data - pandas dataframe
+            f1-f3  data features
+            age_bin column
+            income_bin column
+    Returns:
+            figure with 4 subplots
+    '''
+    fig, ((ax1, ax2), (ax3, ax4))= plt.subplots(2,2, figsize=(30,20))
+
+    data[[f1, f2]].groupby([f1]).mean().plot.area(alpha = 0.2, ax = ax1, color = 'b');
+    ax1.set_title('Serious Financial Distress by No Dependents');
+
+
+    data[[f2, age_bin]].groupby([age_bin]).mean().plot.area(alpha = 0.2, ax = ax2, color = 'r');
+    ax2.set_title('Serious Financial Distress by Age');
+
+
+    data[[f2, income_bin]].groupby([income_bin]).mean().plot.area(alpha = 0.2, ax = ax3, color = 'purple');
+    ax3.set_title('Serious Financial Distress by Monthly Income');
+
+
+    data[[f3, age_bin]].groupby([age_bin]).mean().plot.area(alpha = 0.2, ax = ax4, color = 'green');
+    ax4.set_title('Debt/ Income Ratio by Age');
+
+    fig.tight_layout()
+
+
+def plot_corr_matrix(data):
+    '''
+    Heatmap of correlation matrix
+    Inputs: dataframe
+    Returns: Heatmap
+            (Green + corr. Red - corr.)
+    '''
+    sns.set(font_scale=1.4)#for label size
+    ax = plt.axes()
+    sns.heatmap(data.corr(), square=True, cmap='RdYlGn')
+    ax.set_title('Correlation Matrix')
